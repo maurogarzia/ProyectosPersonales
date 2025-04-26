6ba8f330a779170b9ec2f35a386e3c3450347874
maurogarzia <maurogarzia2@gmail.com>
2025-04-26 14:43:40 -0300

    Subo tarjeta terminada a excepcion de sitio web

--- TarjetaInteractiva/interactiveCard/src/components/Screen/Screen.tsx
+++ TarjetaInteractiva/interactiveCard/src/components/Screen/Screen.tsx
@@ -3,29 +3,46 @@ import { useState } from 'react'
 import { Card } from '../ui/Card/Card'
 import { CardReverse } from '../ui/CardReverse/CardReverse'
 import styles from './Screen.module.css'
+import { motion, AnimatePresence } from "framer-motion";
 
 export const Screen = () => {
 
     const [viewCard, setViewCard] = useState(false) // Estado para mostrar los componentes
 
-    
-
-
     return (
         <div className={styles.containerPrincipal}>
-            <button className={styles.leftArrow} onClick={() => setViewCard(false)}>
-                <span className="material-symbols-outlined">
-                    arrow_back_ios
-                </span>
+            <button className={styles.spinButton} onClick={() => setViewCard(!viewCard)}>
+                <p>Girar</p>
             </button>
+
             <div>
-                {viewCard ? <Card/> : <CardReverse/>}
+                <AnimatePresence mode='wait'>
+                    {viewCard ? (
+                        <motion.div
+                            key="reverse"
+                            initial={{opacity:1 , rotateY: 90}}
+                            animate={{opacity: 1, rotateY : 0}}
+                            exit={{opacity:1, rotateY: -90}}
+                            transition={{duration : 0.2}}
+                            style={{perspective : '2000px'}}
+                        >
+                            <CardReverse/>
+                        </motion.div>
+                    ): (
+                        <motion.div
+                            key='front'
+                            initial={{opacity : 1, rotateY: 90}}
+                            animate ={{opacity : 1, rotateY : 0}}
+                            exit={{opacity: 1 , rotateY : -90}}
+                            transition={{duration : 0.2}}
+                            style={{perspective : '2000px'}}
+                        >
+                            <Card/>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
             </div>
-            <button className={styles.rigthArrow} onClick={() => setViewCard(true)}>
-                <span className="material-symbols-outlined">
-                    arrow_forward_ios
-                </span>
-            </button>
+            
         </div>
     )
 }
